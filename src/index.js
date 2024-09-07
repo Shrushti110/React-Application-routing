262608f78da4080ffc586cbbbab85dad3ffad6f6
Shrushti110 <giramkarshrushti@gmail.com>
2024-09-07 20:33:42 +0530

    new packages install

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import "./index.css"
 
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+
 import Home from "./views/Home/home"
 import About from "./views/About/about"
 import Contact from "./views/Contact/contact"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const currentPath  = window.location.pathname;
-
-if(currentPath=="/")
-    {
-    root.render(<Home/>)
-}
-else if(currentPath=="/about")
-{
-    root.render(<About/>)
-}
-else if(currentPath=="/contact")
-{
-    root.render(<Contact/>)
-}
-else
-{
-    root.render(<h1>404 Not Found</h1>)
-}
-
-
-
+const router =  createBrowserRouter()
