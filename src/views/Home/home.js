105ed783b9ee55008ae2dcbfcd60445e925ce3b4
Shrushti110 <giramkarshrushti@gmail.com>
2024-09-07 21:03:33 +0530

    button created

--- src/views/Home/home.js
+++ src/views/Home/home.js
@@ -3,6 +3,7 @@ import Navbar from "./../../components/Navbar/navbar"
 import HomeImg from "./House (2).png"
 import "./Home.css"
 import Footer from '../../Footer/Footer'
+import  Toaster  from "react-hot-toast"
 
 function home() {
   return (
@@ -11,6 +12,12 @@ function home() {
         <h1 className='title'>Home</h1>
 
         <img src={HomeImg} className='img'/>
+
+        <button type="button" onClick={()=>{
+          
+        }}>
+          Show Toast 
+        </button>
 
         <Footer/>
     </div>
